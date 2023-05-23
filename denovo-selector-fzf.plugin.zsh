b4f2d0af03f7b92276ab3ae1a876b7a2cb15980a
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-23 13:08:19 +0900

    fix: use denovo_dispatch because of notity removal

--- denovo-selector-fzf.plugin.zsh
+++ denovo-selector-fzf.plugin.zsh
@@ -2,6 +2,6 @@ typeset -gaU DENOVO_PATH
 DENOVO_PATH+=("${0:a:h}")
 
 function denovo-fzf-ghq() {
-  denovo_notify denovo-selector-fzf ghq_cd
+  denovo_dispatch denovo-selector-fzf ghq_cd >/dev/null
 }
 zle -N denovo-fzf-ghq
